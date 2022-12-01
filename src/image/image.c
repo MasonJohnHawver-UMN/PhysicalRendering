5870e2b211a97a7b874fe37ffd6f6346e2fa4514
Mason Hawver <mason.hawver@gmail.com>
2022-11-30 18:09:18 -0600

    image (ppm) library done

--- src/image/image.c
+++ src/image/image.c
@@ -1,28 +1,93 @@
-#include <stdlib.h>
-#include <stdio.h>
-
-typedef struct { uint8_t r, g, b; } pixel_t;
-typedef struct { pixel_t* pixels; size_t width, height; } bitmap_t;
+#include <image/image.h>
 
 //...
 
-int main(void)
+/************************ private functions ****************************/
+
+static void
+die(const char* message)
 {
-  const int dimx = 800, dimy = 800;
-  int i, j;
-  FILE *fp = fopen("first.ppm", "wb"); /* b - binary mode */
-  fprintf(fp, "P6\n%d %d\n255\n", dimx, dimy);
-  for (j = 0; j < dimy; ++j)
-  {
-    for (i = 0; i < dimx; ++i)
+  fprintf(stderr, "ppm: %s\n", message);
+  exit(1);
+}
+
+static void
+ppm_header(FILE *fp, int *width, int *height)
+{
+  char ch; int  maxval;
+
+  if (fscanf(fp, "P%c\n", &ch) != 1 || ch != '6') { die("file is not in ppm raw format; cannot read"); }
+
+  /* skip comments */
+  ch = getc(fp);
+  while (ch == '#')
     {
-      static unsigned char color[3];
-      color[0] = i % 256;  /* red */
-      color[1] = j % 256;  /* green */
-      color[2] = (i * j) % 256;  /* blue */
-      (void) fwrite(color, 1, 3, fp);
+      do {
+	       ch = getc(fp);
+      } while (ch != '\n');
+      ch = getc(fp);
     }
-  }
-  (void) fclose(fp);
-  return EXIT_SUCCESS;
+
+  if (!isdigit(ch)) { die("cannot read header information from ppm file"); }
+  ungetc(ch, fp); /* put that digit back */
+
+  /* read the width, height, and maximum value for a pixel */
+  if (fscanf(fp, "%d%d%d\n", width, height, &maxval) != 3) { die("file is not in ppm raw format; cannot read"); };
+  if (maxval != 255) { die("image is not true-color (24 bit); read failed"); }
+}
+
+/************************ public functions ****************************/
+
+void alloc_bitmap(bitmap_t* bitmap, size_t width, size_t height)
+{
+  bitmap->width  = width;
+  bitmap->height = height;
+  bitmap->pixels = (pixel_t*) malloc(width * height * sizeof(pixel_t));
+  if (!bitmap->pixels) { die("cannot allocate memory for new image"); }
+}
+
+void free_bitmap(bitmap_t* bitmap)
+{
+  if (bitmap->pixels) { free(bitmap->pixels); }
+  bitmap->width = 0;
+  bitmap->height = 0;
+}
+
+pixel_t* pixel_bitmap(bitmap_t* bitmap, size_t row, size_t col)
+{
+  return bitmap->pixels + row * bitmap->width + col;
+}
+
+void save_bitmap(bitmap_t* bitmap, const char* path)
+{
+  int num;
+  int size = bitmap->width * bitmap->height;
+
+  FILE *fp = fopen(path, "w");
+  if (!fp) { die("cannot open file for writing"); }
+
+  fprintf(fp, "P6\n%d %d\n%d\n", (int)bitmap->width, (int)bitmap->height, 255);
+
+  num = fwrite((void *) bitmap->pixels, sizeof(pixel_t), (size_t)size, fp);
+
+  if (num != size) { die("cannot write image data to file"); }
+  fclose(fp);
+}
+
+void load_bitmap(bitmap_t* bitmap, const char* path)
+{
+  int width, height, num, size;
+  FILE *fp = fopen(path, "r");
+  if (!fp) { die("cannot open file"); }
+
+  ppm_header(fp, &width, &height);
+
+  free_bitmap(bitmap);
+  alloc_bitmap(bitmap, width, height);
+
+  size = width * height;
+  num  = fread((void *) bitmap->pixels, sizeof(pixel_t), (size_t)size, fp);
+
+  if (num != size) { die("cannot read image data from file"); }
+  fclose(fp);
 }
